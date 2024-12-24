0279b3c6214e7dab974b8c2846d12d5f650a0151
Morel Takougang <morelt98@gmail.com>
2024-12-23 23:18:08 -0800

    Improve random event generator

--- Sources/Owl/Util/RandomEventGenerator.swift
+++ Sources/Owl/Util/RandomEventGenerator.swift
@@ -1,21 +1,24 @@
 import Foundation
 
+// Generates a bunch of events by:
+// - Creating the events with random event names
+// - For each event, it creates a random
 public class RandomEventGenerator {
     private var nEvents: Int
+    private var maxNStepsPerEvent: Int
+    private var maxNLabelsPerStep: Int
     private var events: [EventInstance]
     
-    public init(nEvents: Int? = nil) {
-        if nEvents != nil {
-            self.nEvents = nEvents!
-        } else {
-            self.nEvents = Int.random(in: 5..<11)
-        }
+    public init(nEvents: Int = 10, maxNStepsPerEvent: Int = 10, maxNLabelsPerStep: Int = 10) {
+        self.nEvents = nEvents
+        self.maxNStepsPerEvent = maxNStepsPerEvent
+        self.maxNLabelsPerStep = maxNLabelsPerStep
         self.events = []
     }
 
     public func run() {
         generateEvents()
-        generateStepsAndLabels()
+        generateSteps()
         closeEvents()
         
         print("Sleeping to let events being sent to the server...")
@@ -25,9 +28,18 @@ public class RandomEventGenerator {
     private func generateEvents() {
         print("Generating events...")
         for i in 0..<nEvents {
+            
+            // Choose a random event to create
+            // Note that this means multiple events of the
+            // same name can be created
             let id = Int.random(in: 1..<nEvents)
             let eventName = "event_" + String(id)
             let event = Owl.newEvent(name: eventName)
+            
+            // Add some delay so that not all the events
+            // start at the same time
+            Thread.sleep(forTimeInterval: TimeInterval(Int.random(in: 1...10)))
+            
             if event.start() {
                 events.append(event)
             }
@@ -36,27 +48,40 @@ public class RandomEventGenerator {
         print("\(nEvents) generated.")
     }
     
-    private func generateStepsAndLabels() {
+    private func generateSteps() {
         print("Generating steps and labels...")
-        for i in 0..<10 {
-            // Choose a random event
-            guard let event = events.randomElement() else {
-                continue
-            }
+        for i in 0..<nEvents {
+            // Pick a number of steps for the current events
+            let nSteps = Int.random(in: 0...maxNStepsPerEvent)
             
-            // Generate steps
-            let stepName = "step_" + String(Int.random(in: 1..<100))
-            event.step(stepName)
-            
-            // Generate Labels
-            for i in 0..<3 {
-                let id = Int.random(in: 1..<10)
-                let key = "key#" + String(id)
-                let val = "val#" + String(id)
-                event.label(key: key, val: val)
+            // generate the steps
+            for _ in 0..<nSteps {
+                // wait a bit
+                Thread.sleep(forTimeInterval: TimeInterval(Int.random(in: 1...5)))
+                
+                // Multiple steps can have the same name
+                // (but they will have different step numbers)
+                let stepName = "step_" + String(Int.random(in: 0...nSteps))
+                events[i].step(stepName)
+                
+                // generate labels
+                generateLabels(for: events[i])
             }
         }
         print("Steps and labels generated.")
+    }
+    
+    private func generateLabels(for event: EventInstance) {
+        // Pick a number of labels for the current event
+        let nLabels = Int.random(in: 0...maxNLabelsPerStep)
+        
+        // generate labels
+        for _ in 0..<nLabels {
+            let i = Int.random(in: 0...nLabels)
+            let key = "key_" + String(i)
+            let val = "val_" + String(i)
+            event.label(key: key, val: val)
+        }
     }
     
     private func closeEvents() {
